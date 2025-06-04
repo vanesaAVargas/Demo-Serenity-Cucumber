2974dee6f4ed9b6137db25d281a4f253188a2778
Vanesa Arteaga <arteagavargasva@gmail.com>
2025-06-04 15:44:17 -0300

    Update Login Steps page

--- src/test/java/com/swag/pe/pages/product/SelectProductsPage.java
+++ src/test/java/com/swag/pe/pages/product/SelectProductsPage.java
@@ -9,6 +9,6 @@ import java.util.List;
 public class SelectProductsPage extends PageObject {
     @FindBy(xpath = "//button[@class='btn_primary btn_inventory']")
     protected List<WebElementFacade> products;
-    @FindBy(xpath = "//*[local-name()='svg']//ancestor::a")
+    @FindBy(xpath = "//*[local-()='svg']//ancestor::a")
     protected WebElementFacade shoppingCartIcon;
 }
